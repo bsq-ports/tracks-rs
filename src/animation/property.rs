bd2115839f089cf4ef1667fc7b7f9d83ea8a2890
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2025-03-11 08:37:43 -0400

    Add interpolation method to PathProperty for smoother transitions

--- src/animation/property.rs
+++ src/animation/property.rs
@@ -1,6 +1,9 @@
 use glam::{Quat, Vec3, Vec4};
 
-use crate::point_definition::{BasePointDefinition, PointDefinition};
+use crate::{
+    point_definition::{BasePointDefinition, PointDefinition},
+    values::{base_provider_context::BaseProviderContext, value::BaseValue},
+};
 
 pub enum Property {
     Float(f32),
@@ -30,6 +33,21 @@ impl PathProperty {
         self.time = 0.0;
         self.prev_point = self.point.take();
         self.point = new_point_data;
+    }
+
+    pub fn interpolate(&self, time: f32, context: &BaseProviderContext) -> Option<BaseValue> {
+        match (&self.prev_point, &self.point) {
+            (Some(prev_point_data), Some(point_data)) => {
+                let a = prev_point_data.interpolate(time, context).0;
+                let b = point_data.interpolate(time, context).0;
+
+                let result = BaseValue::lerp(a, b, self.time);
+
+                Some(result)
+            }
+            (None, Some(point_data)) => Some(point_data.interpolate(time, context).0),
+            _ => None,
+        }
     }
 }
 
